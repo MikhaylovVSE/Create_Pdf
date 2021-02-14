64a965d8aa0e91d9b04e401d8970deedfc140d61
Виктор Михайлов <victormihailov@hotmail.com>
2021-02-14 15:42:11 +0300

    Добавьте файлы проекта.

---
+++ pdf1/Model.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace pdf1
+{
+    public class Report
+    {
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+        public List<Category> Items { get; set; }
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CompanyCount { get; set; }
+    }
+}
---
+++ pdf1/Program.cs
@@ -0,0 +1,96 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace pdf1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+               https://www.c-sharpcorner.com/UploadFile/f2e803/basic-pdf-creation-using-itextsharp-part-i/
+               https://www.c-sharpcorner.com/uploadfile/f2e803/basic-pdf-creation-using-itextsharp-part-ii/
+               https://www.c-sharpcorner.com/uploadfile/f2e803/basic-pdf-creation-using-itextsharp-part-iii/
+               https://www.c-sharpcorner.com/blogs/create-table-in-pdf-using-c-sharp-and-itextsharp
+            */
+
+            var fileName = "test.pdf";
+            using (var stream = new FileStream(fileName, FileMode.Create))
+            {
+                var doc = new Document(PageSize.A4.Rotate());
+                var writer = PdfWriter.GetInstance(doc, stream);
+                doc.Open();
+
+                var data = GetTestData();
+                MakePdf(doc, data);
+
+                doc.Close();
+                writer.Close();
+            }
+            ///Открытие файла для ускорения
+            Process.Start(new ProcessStartInfo(fileName)
+            {
+                UseShellExecute = true
+            });
+        }
+
+        ///Создание pdf-файла и его заполнение
+        static void MakePdf(Document document, Report data)
+        {
+            string ttf = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Fonts), "ARIAL.TTF");
+            var baseFont = BaseFont.CreateFont(ttf, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+            var font = new Font(baseFont, Font.DEFAULTSIZE, Font.NORMAL);
+
+            var title = $"Hello from {data.From.ToShortDateString()} to {data.To.ToShortDateString()}";
+            document.Add(new Paragraph(title, font));
+
+            var table = new PdfPTable(3);
+            table.SpacingBefore = 20;
+            table.WidthPercentage = 100;
+            table.SetWidths(new float[] { 0.4f, 1.0f, 1.0f });
+
+            foreach(var item in data.Items) {
+                table.AddCell($"{item.Id}");
+                table.AddCell($"{item.Name}");
+                table.AddCell($"{item.CompanyCount}");
+            }
+            document.Add(table);
+        }
+
+        static Report GetTestData()
+        {
+            var result = new Report()
+            {
+                From = new DateTime(2021, 01, 01),
+                To = DateTime.Now,
+                Items = new List<Category>()
+                {
+                    new Category()
+                    {
+                        Id = 1,
+                        Name = "Test111",
+                        CompanyCount = 153
+                    },
+                    new Category()
+                    {
+                        Id = 2,
+                        Name = "Test222",
+                        CompanyCount = 153
+                    },
+                    new Category()
+                    {
+                        Id = 3,
+                        Name = "Test333",
+                        CompanyCount = 153
+                    }
+                }
+            };
+            return result;
+        }
+    }
+}
