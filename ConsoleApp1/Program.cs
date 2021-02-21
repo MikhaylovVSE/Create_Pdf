86f2b70686576134c996e78dbc2326e947faf15b
levink <levinkostya@yandex.ru>
2021-02-21 12:25:15 +0300

    Первая задача на ввод-вывод

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("input number >");
+                string input = Console.ReadLine();
+                if (input == "q") {
+                    break;
+                }
+
+                int number = Int32.Parse(input);
+                for(int i = 0; i < number ; i++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            } 
+        }
+    }
+}
